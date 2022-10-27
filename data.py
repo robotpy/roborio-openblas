553d71e512ce6a5ab9dbaf0cc66557d3dc7d4d2e
Dustin Spicuzza <dustin@virtualroadside.com>
2022-10-27 01:05:37 -0400

    Bump libgfortran name

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 name = "robotpy-openblas"
-version = "0.3.21+r1"
+version = "0.3.21+r2"
 url = "https://www.openblas.net"
 license = "BSD"
-install_requires = ["robotpy-libgfortran4"]
+install_requires = ["robotpy-libgfortran5"]
 
 # dev install requires libgfortran-dev, but that's included with the
 # fortran compiler
